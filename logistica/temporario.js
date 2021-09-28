97c6832a87144f3da5f60055694300d017b36f7b
alissonbtt <alison_btt@hotmail.com>
2021-09-27 21:59:02 -0300

    exclui arquivos temporarios

--- logistica/temporario.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const gets = require("prompt-sync")();
-
-let name = gets('Digite seu nome? ');
-// Usuário digita "Carlos".
-
-console.log('Hello, ' + name + '!');
-// -> Hello, Carlos!
